8754041c4a7236e18fb13f01efd2476903ca96aa
giorgixomeriki <95750270+giorgixomeriki@users.noreply.github.com>
2024-09-23 18:26:12 +0400

    v

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert("rame")
+const btn = document.querySelectorAll(".navbar ul li");
+let text = document.querySelector('h1')
+
+btn.forEach(button => {
+    button.addEventListener('click', (event) => {
+        text.textContent = event.target.textContent
+
+        switch (text.textContent) {
+            case "Home": text.style.color = "red"
+                break;
+            case "About": text.style.color = "green"
+                break;
+            case "Contact": text.style.color = "yellow"
+                break;
+            case "Support": text.classList.add('testClass')
+                break;
+            case "Search": text.classList.remove('testClass')
+        } 
+    })
+}) 
